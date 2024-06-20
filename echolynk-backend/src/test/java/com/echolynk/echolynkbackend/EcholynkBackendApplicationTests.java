9ee4875a1a4b29b7db12aceea4f9a1d653424ed6
Wikumbandara <wikumpreethika123@gmail.com>
2024-06-20 23:45:05 +0530

    Web Project intiated

---
+++ echolynk-backend/src/test/java/com/echolynk/echolynkbackend/EcholynkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echolynk.echolynkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcholynkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
