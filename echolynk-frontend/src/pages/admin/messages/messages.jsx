3ae1884516fdb4e775f2f7e70904cb4611ea712b
Wikumbandara <wikumpreethika123@gmail.com>
2024-07-03 20:53:15 +0530

    Update Page Names

--- echolynk-frontend/src/pages/admin/messages/messages.jsx
+++ echolynk-frontend/src/pages/admin/messages/messages.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AdminMessages = () => {
     return (
         <div className="flex flex-col justify-start items-start h-full mt-20 ml-4">
-            <h1 className="text-3xl font-semibold mb-4">Admin Dashboard</h1>
+            <h1 className="text-3xl font-semibold mb-4">Messages</h1>
         </div>
     );
 }
